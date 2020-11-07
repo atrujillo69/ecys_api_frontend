0568bb5393cf2293a1c5465283907a891113f834
Aarón Trujillo <atrujilloa6@gmail.com>
2020-11-07 16:53:37 -0600

    2.1

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './components/adming/list/list.component';
+import { DetailsComponent } from './components/details/details.component';
+import { HomeComponent } from './components/home/home.component';
+import { Page404Component } from './components/page404/page404.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { RegisterComponent } from './components/user/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  
+  { path: 'course/:id', component: DetailsComponent },
+  { path: 'admin/list', component: ListComponent},
+  { path: 'user/login', component: LoginComponent},
+  { path: 'user/register', component: RegisterComponent },
+  { path: 'user/profile', component: ProfileComponent}, // TODO: only users auth
+  { path: '**', component: Page404Component }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
