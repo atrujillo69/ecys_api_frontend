61dadf3aea89de72fbd4a0e9ea66e62ff09138ac
Alejandro Barrios <wabarrios@digitalgeko.com>
2020-11-09 23:16:35 -0600

    Authorization headers added

--- client/src/app/services/data-api.service.ts
+++ client/src/app/services/data-api.service.ts
@@ -9,7 +9,11 @@ export class DataApiService {
 
   constructor(private http: HttpClient) { }
   getAllCourses(){
-    const url = 'http//localhost:3000/api/courses'
-    return this.http.get(url)
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImNhcm5ldCI6MjAxNjAyNzM0LCJub21icmVzIjoiQWxlamFuZHJvIiwiYXBlbGxpZG9zIjoiQmFycmlvcyIsImNvcnJlbyI6IjMwMDExNjEwMTAxMDFAaW5nZW5pZXJpYS51c2FjLmVkdS5ndCJ9LCJpYXQiOjE2MDQ5ODUwNjl9.RL7CkuiBkqz0me4gWKfFD41byqTO9JSLtPVG6633Nrk");
+    const url = 'http://localhost:3000/api/courses'
+    return this.http.get(url, {
+      headers: headers
+    });
   }
 }
