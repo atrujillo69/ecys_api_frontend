44f750bf90592e1725f2c355fe2f96f5476a7a4c
Aarón Trujillo <atrujilloa6@gmail.com>
2020-11-10 19:11:18 -0600

    metodos de login y registro
    video 4

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders} from "@angular/common/http";
+import { Observable } from "rxjs/internal/Observable";
+import { map } from "rxjs/operators";
+import { isNullOrUndifined } from "util";
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private htttp: HttpClient) { }
+  headers : HttpHeaders = new HttpHeaders({
+    "content-type": "aplication/json"
+  });
+
+  registerUser(carnet : number, nombres: string, apellidos : string, contraseña: string, email: string ){
+    const url = "http://localhost:3000/api/user/register";
+    return this.htttp.post(url,{
+      carnet: carnet,
+      nombres:nombres, 
+      apellidos:apellidos, 
+      contraseña: contraseña, 
+      email:email
+    },{headers: this.headers}
+    )
+    .pipe(map(data =>data));
+  }
+
+  loginUser(carnet: number, contraseña: string): Observable<any> {
+    const url = "http://localhost:3000/api/user/login?include=user";
+    return this.htttp.post(
+      url,
+      { carnet: carnet, contraseña: contraseña },
+      { headers: this.headers }
+      )
+      .pipe(map(data => data));
+  }
+  setUser(user): void {
+    let user_string = JSON.stringify(user);
+    localStorage.setItem("currentUser", user_string)
+  }
+  
+  setToken(token): void {
+    localStorage.setItem("accessToken", token)
+  }
+
+  getToken() {
+    return localStorage.getItem("accessToken")
+  }
+
+  getCurrentUser(){
+    let user_string = localStorage.getItem('currentUser')
+    if(!isNullOrUndifined(user_string)){
+      let user = JSON.parse(user_string);
+      return user;
+    }else{
+      return null;
+    }
+  }
+
+  logoutUser() {
+    let accessToken = localStorage.getItem("accessToken");
+    const url = `http://localhost:3000/api/Users/logout?access_token=${accessToken}`;
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("currentUser");
+    return this.htttp.post(url, { headers: this.headers });
+  }
 }
--- client/src/app/services/data-api.service.ts
+++ client/src/app/services/data-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { Observable } from "rxjs/internal/Observable"
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from "rxjs/internal/Observable";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,7 @@ export class DataApiService {
   getAllCourses(){
     let headers: HttpHeaders = new HttpHeaders();
     headers = headers.append("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImNhcm5ldCI6MjAxNjAyNzM0LCJub21icmVzIjoiQWxlamFuZHJvIiwiYXBlbGxpZG9zIjoiQmFycmlvcyIsImNvcnJlbyI6IjMwMDExNjEwMTAxMDFAaW5nZW5pZXJpYS51c2FjLmVkdS5ndCJ9LCJpYXQiOjE2MDQ5ODUwNjl9.RL7CkuiBkqz0me4gWKfFD41byqTO9JSLtPVG6633Nrk");
-    const url = 'http://localhost:3000/api/courses'
+    const url = 'http://localhost:3000/api/courses' 
     return this.http.get(url, {
       headers: headers
     });
